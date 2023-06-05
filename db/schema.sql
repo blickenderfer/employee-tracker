41be7ad923c14bac284940946fb83fd1bfeee4d7
Blickenderfer <ecblickenderfer@gmail.com>
2023-06-05 13:49:39 -0400

    started initial schema structure

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+/* this is where databases/tables will be created */
+/* create database at the top of file!! */
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(30) NOT NULL
+); 
+
+/* role table, employee tables */
