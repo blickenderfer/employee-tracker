a8a4fa162e962c0db6feef1782689e37aa42131c
Blickenderfer <ecblickenderfer@gmail.com>
2023-06-09 17:59:08 -0400

    IT WORKS

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ let rows = []
 let inqOutput = []
 if (selection === "Quit"){
     process.exit()
-} else if (selection === "View All Departments") {
+} else if (selection === "View all Departments") {
     rows = await db.query("SELECT * FROM department;");
     console.table(rows[0])
-} else if (selection === "View All Roles") {
+} else if (selection === "View all Roles") {
     rows = await db.query("SELECT role.id, title, salary, department.name AS department FROM role JOIN department ON role.department_id = department.id;");
     console.table(rows[0])
-} else if (selection === "View All Employees") {
+} else if (selection === "View all Employees") {
     rows = await db.query("SELECT employee.id, employee.first_name, employee.last_name, role.title AS title, department.name AS department, role.salary AS salary, CASE WHEN employee.manager_id IS NOT NULL THEN CONCAT(manager_table.first_name, ' ', manager_table.last_name)ELSE NULL END AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id JOIN employee manager_table ON employee.manager_id = manager_table.id;")
     console.table(rows[0])
 } else if (selection === "Add Department") {
@@ -49,14 +49,14 @@ if (selection === "Quit"){
             message: "What department does the new role belong to?"
         } 
     ])
-    const deptId = await db.query(`SELECT IFNULL ((SELECT id FROM department WHERE name = "${roleDepartment}"), "That department does not exist");`)
+    const deptId = await db.query(`SELECT IFNULL ((SELECT id FROM department WHERE name = "${inqOutput.roleDepartment}"), "That department does not exist");`)
     rows = deptId[0]
     const dId = Object.values(rows[0])[0]
     if (dId == "That department does not exist"){
         console.log("No such department.")
         return
     } else (
-        await db.query(`INSERT INTO role (title, salary, department_id) VALUES('${roleName}', '${roleSalary}', '${dId}');`)
+        await db.query(`INSERT INTO role (title, salary, department_id) VALUES('${inqOutput.roleName}', '${inqOutput.roleSalary}', '${dId}');`)
     )
 
 }   else if (selection === "Add Employee") {
@@ -81,7 +81,7 @@ if (selection === "Quit"){
     const everyRole = await db.query(`SELECT * FROM role;`)
     const everyMgr = await db.query(`SELECT * FROM employee WHERE manager_id IS NULL;`)
     const roleData = everyRole[0].filter(role => role.title == inqOutput.role)
-    const mgrData = everMgr[0].filter(manager => `${manager.first_name} ${manager.last_name}` == inqOutput.role)
+    const mgrData = everyMgr[0].filter(manager => `${manager.first_name} ${manager.last_name}` == inqOutput.manager)
     await db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${inqOutput.firstName}', '${inqOutput.lastName}', ${roleData[0].id}, ${mgrData[0].id})`); 
  
 }   else if (selection === "Update Employee Role") {
@@ -99,42 +99,36 @@ if (selection === "Quit"){
             value: role.id
         }
     })
-    
-}
-
-
-
-
-
-
-
-}catch(err){console.log(err)}
-}
-// db.connect((err) => {
-//     if(err)throw err;
-//     menu(); 
-// });  
-connectDb()
+    inqOutput = await inquirer.prompt([
+        {
+            type: "list",
+            name: "empID",
+            message: "Which employee do you want to update?",
+            choices: empList
+        },
+        {
+            type: "list",
+            name: "updatedRole",
+            message: "What is the employees new role?",
+            choices: roleList
+        }
+    ])
+    await db.query (`UPDATE employee SET role_id = ${inqOutput.updatedRole} WHERE employee.id = ${inqOutput.empID}`)
+}}catch(err){console.log(err)}
+} 
+// connectDb()
 function menu(){
     inquirer.prompt([
         {
             type: "list",
             name: "option",
             message: "Please select from the following options.",
-            choices: ["View all Departments", "View all Roles", "View all Employees"]
+            choices: ["View all Departments", "View all Roles", "View all Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role", "Quit"]
         }
-    ]).then((answers)=>{
-        if(answers.option === "View all Departments"){
-            viewDepartments()
-        } else if(answers.option === "View all Roles"){
-            //execute view role function
-        }
+    ]).then(async (answers)=>{
+     await connectDb(answers.option)
+     menu()
     }) 
 }
-//grabbing from database
-async function viewDepartments(){
-    const allDepartments = await db.query("SELECT * FROM departments")
-        console.table(allDepartments)
-        menu()
-    }
+
 menu()
