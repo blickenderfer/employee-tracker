0337d2bb3a6705a05f8f92e8eda0fe7aba745300
Blickenderfer <ecblickenderfer@gmail.com>
2023-06-09 13:05:50 -0400

    beginning queries

--- index.js
+++ index.js
@@ -3,18 +3,43 @@ const inquirer = require("inquirer");
 require("dotenv").config()
 
 // create the connection to database
-const db = mysql.createConnection({
+async function connectDb(selection){
+    try{
+const db = await mysql.createConnection({
     host: 'localhost',
     user: process.env.DB_USER,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PW
 });
 
-db.connect((err) => {
-    if(err)throw err;
-    menu(); 
-});  
+let rows = []
+let inqOutput = []
+if (selection === "Quit"){
+    process.exit()
+} else if (selection === "View All Departments") {
+    rows = await db.query("SELECT * FROM department");
+    console.table(rows[0])
+} else if (selection === "View All Roles") {
+    rows = await db.query("SELECT role.id, title, salary, department.name AS department FROM role JOIN department ON role.department_id = department.id");
+    console.table(rows[0])
+} else if (selection === "View All Employees") {
+    rows = await db.query("SELECT employee.id, employee.first_name, employee.last_name, role.title AS title, department.name AS department, role.salary AS salary, CASE WHEN employee.manager_id IS NOT NULL THEN CONCAT(manager_table.first_name, ' ', manager_table.last_name)ELSE NULL END AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id JOIN employee manager_table ON employee.manager_id = manager_table.id")
+    console.table(rows[0])
+}
 
+
+
+
+
+
+
+}catch(err){console.log(err)}
+}
+// db.connect((err) => {
+//     if(err)throw err;
+//     menu(); 
+// });  
+connectDb()
 function menu(){
     inquirer.prompt([
         {
@@ -37,4 +62,4 @@ async function viewDepartments(){
         console.table(allDepartments)
         menu()
     }
-
+menu()
